4f896dca52735c66cfa64f3921f31ec26ae31da4
hendisantika <hendisantika@yahoo.co.id>
2023-12-20 15:09:40 +0700

    Add getFilePath in VideoStreamService 🫸🌀✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/videostreaming/service/VideoStreamService.java
+++ src/main/java/com/hendisantika/videostreaming/service/VideoStreamService.java
@@ -6,8 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -111,4 +113,16 @@ public class VideoStreamService {
             return result;
         }
     }
+
+    /**
+     * Get the filePath.
+     *
+     * @return String.
+     */
+    private String getFilePath() {
+        URL url = this.getClass().getResource(VIDEO);
+        assert url != null;
+        return new File(url.getFile()).getAbsolutePath();
+    }
+
 }
