0c6dd3b000b3f576795865d1edc1e0aa002640e0
hendisantika <hendisantika@yahoo.co.id>
2023-12-20 15:08:45 +0700

    Add prepareContent in VideoStreamService 🫸🌀✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/videostreaming/service/VideoStreamService.java
+++ src/main/java/com/hendisantika/videostreaming/service/VideoStreamService.java
@@ -1,7 +1,14 @@
 package com.hendisantika.videostreaming.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+import static com.hendisantika.videostreaming.config.ApplicationConstants.*;
+import static jdk.jfr.internal.SecuritySupport.getFileSize;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +23,55 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class VideoStreamService {
+    /**
+     * Prepare the content.
+     *
+     * @param fileName String.
+     * @param fileType String.
+     * @param range    String.
+     * @return ResponseEntity.
+     */
+    public ResponseEntity<byte[]> prepareContent(final String fileName, final String fileType, final String range) {
+        try {
+            final String fileKey = fileName + "." + fileType;
+            long rangeStart = 0;
+            long rangeEnd = CHUNK_SIZE;
+            final Long fileSize = getFileSize(fileKey);
+            if (range == null) {
+                return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                        .header(CONTENT_TYPE, VIDEO_CONTENT + fileType)
+                        .header(ACCEPT_RANGES, BYTES)
+                        .header(CONTENT_LENGTH, String.valueOf(rangeEnd))
+                        .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+                        .header(CONTENT_LENGTH, String.valueOf(fileSize))
+                        .body(readByteRangeNew(fileKey, rangeStart, rangeEnd)); // Read the object and convert it as bytes
+            }
+            String[] ranges = range.split("-");
+            rangeStart = Long.parseLong(ranges[0].substring(6));
+            if (ranges.length > 1) {
+                rangeEnd = Long.parseLong(ranges[1]);
+            } else {
+                rangeEnd = rangeStart + CHUNK_SIZE;
+            }
 
+            rangeEnd = Math.min(rangeEnd, fileSize - 1);
+            final byte[] data = readByteRangeNew(fileKey, rangeStart, rangeEnd);
+            final String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+            HttpStatus httpStatus = HttpStatus.PARTIAL_CONTENT;
+            if (rangeEnd >= fileSize) {
+                httpStatus = HttpStatus.OK;
+            }
+            return ResponseEntity.status(httpStatus)
+                    .header(CONTENT_TYPE, VIDEO_CONTENT + fileType)
+                    .header(ACCEPT_RANGES, BYTES)
+                    .header(CONTENT_LENGTH, contentLength)
+                    .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
+                    .body(data);
+        } catch (IOException e) {
+            logger.error("Exception while reading the file {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
+    }
 }
