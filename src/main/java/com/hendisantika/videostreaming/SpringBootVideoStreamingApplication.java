3f1323030e76c958babadffccf0f7c08618c54d1
hendisantika <hendisantika@yahoo.co.id>
2023-12-20 15:00:57 +0700

    Create starter project 🫸🌀✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/videostreaming/SpringBootVideoStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.videostreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVideoStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootVideoStreamingApplication.class, args);
+    }
+
+}
