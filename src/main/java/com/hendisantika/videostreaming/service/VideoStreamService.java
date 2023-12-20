5fb6616e16720bb551a9b0a42e274ad84a3329f7
hendisantika <hendisantika@yahoo.co.id>
2023-12-20 15:05:47 +0700

    Add VideoStreamService 🫸🌀✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/videostreaming/service/VideoStreamService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.videostreaming.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-video-streaming
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/20/23
+ * Time: 15:05
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+public class VideoStreamService {
+
+}
